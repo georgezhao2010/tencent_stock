12b816a5efea9345c654d43cdb709edd0aa12fc9
George Zhao <involute@sina.com>
2021-07-01 10:57:24 +0800

    First Commit and Set version to v0.0.2

---
+++ tencent_stock/const.py
@@ -0,0 +1,7 @@
+DOMAIN = "tencent_stock"
+REPOS_API_URL = "https://qt.gtimg.cn/q="
+CONF_EXCHANGE = "exchange"
+CONF_STOCK = "stock"
+MIN_UPDATE_INTERVAL = 10
+MAX_UPDATE_INTERVAL = 36000
+TIME_SLICES = [{"begin_time": "9:15", "end_time": "11:30"}, {"begin_time": "13:00", "end_time": "15:00"}]
